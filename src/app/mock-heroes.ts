1428bda791f39bfc77b64f8b672a45eedef02d29
anthonycc2 <anthonycc2@users.noreply.github.com>
2020-12-10 19:58:07 -0300

    Primeiro Commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Wolverine' },
+    { id: 12, name: 'Ciplope' },
+    { id: 13, name: 'Noturno' },
+    { id: 14, name: 'Colossus' },
+    { id: 15, name: 'Lince Negra' },
+    { id: 16, name: 'Fenix' },
+    { id: 17, name: 'Banshee' },
+    { id: 18, name: 'Apache' },
+    { id: 19, name: 'Fera' },
+    { id: 20, name: 'Homem de Gelo' }
+];
