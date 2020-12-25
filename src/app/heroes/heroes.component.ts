0cdac303ad59f4d315b1da2a281a76b19f71f5cd
anthonycc2 <anthonycc2@users.noreply.github.com>
2020-12-25 17:57:20 -0300

    7º commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -49,4 +49,9 @@ export class HeroesComponent implements OnInit {
       });
   }
 
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
 }
